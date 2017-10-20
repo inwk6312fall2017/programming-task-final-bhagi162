d8f7d3d414b7b45c54001c00b88c4ce611ae267e
bhagi162 <bhagirath.patel@dal.ca>
2017-10-20 17:00:51 +0000

    making the all file read

---
+++ task1.py
@@ -0,0 +1,10 @@
+book1=open("Book1.txt",'r')
+book2=open("Book2.txt",'r')
+book3=open("Book3.txt",'r')
+
+line=book1.read()
+line=book2.read()
+line=book3.read()
+word=line.strip(" ")
+nn=word.split()
+print(nn)
